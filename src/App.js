834822e70b238339bc237000b3f8529a16c7e0d0
kristofer11 <klhvattum@live.com>
2022-12-10 13:27:03 -0800

    Week 2 workshop complete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   console.log();
@@ -15,7 +17,12 @@ function App() {
       <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='contact' element={<ContactPage/>} />
+          <Route path='about' element={<AboutPage />} />          
           <Route path='directory' element={<CampsitesDirectoryPage />} />
+          <Route 
+            path='directory/:campsiteId'
+            element={<CampsiteDetailPage />} 
+          />
       </Routes>
       <Footer/>
     </div>
